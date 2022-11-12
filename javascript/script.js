b9413011ee4be775dc065207b5d0c9b6e19e4efc
John Salyer <johndsalyer@gmail.com>
2022-11-11 20:03:47 -0500

    buttons added colors added

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,11 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  
+  const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
